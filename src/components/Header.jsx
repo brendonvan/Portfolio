d4a7ea945f7bc3155fe0616103257b7200a5ed1d
brendonvan <brendon@van.digital>
2022-10-26 04:02:09 -0700

    Change color theme and svg logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,8 @@ const Header = () => {
     return (
         <div className='header-container'>
             <div className='header'>
-                <div className='header-left'><p>Brendon Van</p></div>
+                <div className='header-left'><img src="./icons/BVFilm-source.svg" alt="logo" /></div>
+                
                 <div className='header-right'><img src="./icons/menu.svg" alt="menu" /></div>
             </div>
         </div>

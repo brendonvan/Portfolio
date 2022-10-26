92224b8b56e26cdfbe63663ef6c7e04cb6366d05
brendonvan <brendon@van.digital>
2022-10-26 08:03:06 -0700

    Complete Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,32 @@
 import './Header.css';
+import { useState, useEffect } from 'react'
+
 
 const Header = () => {
+    let [menuOpen, setMenuOpen] = useState(false);
+
+    function toggleMenu() {
+        console.log(menuOpen)
+        if (!menuOpen) {
+            setMenuOpen(true);
+            
+        } else {
+            setMenuOpen(false);
+        }
+    }
+
     return (
         <div className='header-container'>
             <div className='header'>
                 <div className='header-left'><img src="./icons/BVFilm-source.svg" alt="logo" /></div>
                 
-                <div className='header-right'><img src="./icons/menu.svg" alt="menu" /></div>
+                <div className={ menuOpen ? 'header-right open' : 'header-right' }>
+                    <div className='header-menu' onClick={ () => { toggleMenu() } }>
+                        <span className='line-1'></span>
+                        <span className='line-2'></span>
+                        <span className='line-3'></span>
+                    </div>
+                </div>
             </div>
         </div>
     )
